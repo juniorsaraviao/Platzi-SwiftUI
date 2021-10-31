776373ddf7e12f70bc1a4d66ed9779736af08cff
Junior Saravia <junior.saravia.o@uni.pe>
2021-10-31 18:10:55 -0500

    Initial Commit

---
+++ Shared/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Shared
+//
+//  Created by Junior Eduardo Saravia Oria on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
