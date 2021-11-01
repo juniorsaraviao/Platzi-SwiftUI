6c6e48deed960f690fb291e695eca9b40a50c2bc
Junior Saravia <junior.saravia.o@uni.pe>
2021-10-31 19:30:24 -0500

    First steps

---
+++ Shared/Buttons.swift
@@ -0,0 +1,23 @@
+//
+//  Buttons.swift
+//  Platzi
+//
+//  Created by Junior Eduardo Saravia Oria on 31/10/21.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
